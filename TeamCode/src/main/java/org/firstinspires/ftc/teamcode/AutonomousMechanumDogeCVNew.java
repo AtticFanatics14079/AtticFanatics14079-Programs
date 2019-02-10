5d3817f985b808bb183e8a064ee9ac006b9e5120
JonCoombs <46505004+JonCoombs@users.noreply.github.com>
2019-02-10 15:35:08 -0500

    Kool.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMechanumDogeCVNew.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMechanumDogeCVNew.java
@@ -56,7 +56,7 @@ import java.util.Locale;
  * @see <a href="http://www.adafruit.com/products/2472">Adafruit IMU</a>
  */
 
-@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "AutonomousMechanumDogeCVCraterSide", group = "Sensor")
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "AutonomousMechanumDogeCVCraterSideKoolVersion", group = "Sensor")
 //@Disabled
 public class AutonomousMechanumDogeCVNew extends LinearOpMode
 {
